53797a2b3a8932e21ad68fcb49c4f2dc89c78c0f
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-30 16:47:47 -0300

    Import helper functions to PlayerList component

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,10 +1,13 @@
 import playerData from '../data/playerData';
 import matchData from '../data/matchData';
+import { preparePlayerData, addWinsToPlayer } from '../helpers/playerHelpers';
 import Player from './Player';
 
-function PlayerList() {
+function PlayerList(props) {
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayer(playerDataArray, matchData);
   return (
-    <section className="PlayerList">
+    <section className="PlayerList" >
       <h1>Current participating players</h1>
       <Player />
     </section>
