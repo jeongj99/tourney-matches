0aee39851c584b49da68f15fd2b43978331c21d6
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-30 16:43:45 -0300

    Create helper functions preparePlayerData and addWinsToplayer

---
+++ src/helpers/playerHelpers.js
@@ -0,0 +1,16 @@
+export const preparePlayerData = (playerData) => {
+  return Object.values(playerData);
+};
+
+export const addWinsToPlayer = (playerDataArray, matchData) => {
+  return playerDataArray.map(player => {
+    const currentWins = matchData.reduce((accumulator, match) => {
+      if (match.winner === player.gamerTag) {
+        return accumulator += 1;
+      }
+      return accumulator;
+    }, 0);
+    player.wins = currentWins;
+    return player;
+  });
+};
