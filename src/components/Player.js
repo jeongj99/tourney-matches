721cefdab25aca6a4dc93c70b350b513645afa5c
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-30 15:29:27 -0300

    Create the Player component

---
+++ src/components/Player.js
@@ -0,0 +1,14 @@
+function Player() {
+  return (
+    <article>
+      <h1>
+        John <span>Secret</span> Doe
+      </h1>
+      <h2 className="zero">Currently with no wins :(</h2>
+      <h2>Currently at 1 win</h2>
+      <h2>Currently at 1+ wins</h2>
+    </article>
+  );
+}
+
+export default Player;
