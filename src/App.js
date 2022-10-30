72984b06e8d22c67ef6a6ba038053eb3d051a10d
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-30 17:43:31 -0300

    More refactoring

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import matchData from './data/matchData';
 import playerData from './data/playerData';
 import PlayerList from './components/PlayerList';
 import MatchList from './components/MatchList';
+import { preparePlayerData, addWinsToPlayer } from './helpers/playerHelpers';
 
 function App() {
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayer(playerDataArray, matchData);
   return (
     <div className="App">
-      <h1>Tourney Matches{" "}
-        <span>Where Coding and Tournaments found their Match</span>
+      <h1>Tourney Matches <span>Where Coding and Tournaments found their Match</span>
       </h1>
-      <PlayerList matchData={matchData} playerData={playerData} />
+      <PlayerList playerData={parsedPlayerData} />
       <MatchList matchData={matchData} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,10 +1,7 @@
-import { preparePlayerData, addWinsToPlayer } from '../helpers/playerHelpers';
 import Player from './Player';
 
 function PlayerList(props) {
-  const playerDataArray = preparePlayerData(props.playerData);
-  const parsedPlayerData = addWinsToPlayer(playerDataArray, props.matchData);
-  const onePlayer = parsedPlayerData[0];
+  const onePlayer = props.playerData[0];
   return (
     <section className="PlayerList" >
       <h1>Current participating players</h1>
