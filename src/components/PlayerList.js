657fcd402096c2e75dcf0a1ffae38df7d0b62911
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-30 18:03:57 -0300

    Render all players

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,11 +1,13 @@
 import Player from './Player';
 
 function PlayerList(props) {
-  const onePlayer = props.playerData[0];
+  const players = props.playerData.map(player =>
+    <Player key={player.gamerTag} gamerTag={player.gamerTag} firstName={player.firstName} lastName={player.lastName} wins={player.wins} />
+  );
   return (
     <section className="PlayerList" >
       <h1>Current participating players</h1>
-      <Player firstName={onePlayer.firstName} lastName={onePlayer.lastName} gamerTag={onePlayer.gamerTag} wins={onePlayer.wins} />
+      {players}
     </section>
   );
 }
